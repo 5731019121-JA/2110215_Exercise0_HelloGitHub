29930072814e734f4d87fe9c8df2629eb19c4e65
5731019121-JA <vbssoloww@gmail.com>
2015-08-21 09:57:22 +0700

    Implement greetingMessage

--- src/logic/Hello.java
+++ src/logic/Hello.java
@@ -23,7 +23,9 @@ public class Hello {
 		for(int i=0;i<array.length;i++){
 			for(int j=0;j<array.length-1;j++){
 				if(array[j]>array[j+1]){
-				
+					int tmp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=tmp;
 				}
 			}
 		}
